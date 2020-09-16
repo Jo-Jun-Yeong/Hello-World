f8b8efde943520d34c8758dd0edeec304bc5a8b0
apple <apple@218.38.137.28>
2020-09-16 20:11:51 +0900

    initial commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,15 @@
-
-public class HelloWorld {
-	public static void main(String[] arg) {
-	System.out.println("Hi World");
-	System.out.println("Hello World");
-	}
+	import java.util.Scanner;
+	public class HelloWorld {
+			public static void main(String[] args) {
+				Scanner scanner = new Scanner(System.in);
+				int scan = scanner.nextInt();
+				System.out.println("몇단? :    ");
+				for(int i = 2; i <= scan; i++) {
+					for(int j = 1; j <= scan; j++) {
+						System.out.println("입력값" +scan    );
+						System.out.print(i + "단  ");
+						System.out.println(i * j);
+					}
+				}
+			}
 }
